c6b5de74f4230b476aae0b59f4197d21faf6516f
Vemishettiganesh <vemishettiganesh@gmail.com>
2025-05-05 12:21:21 +0530

    First Commit

---
+++ 22r01a05B6/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $DevOpsCourse.r01a0569;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
